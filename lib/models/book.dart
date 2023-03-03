b3a7619a391c746ee090b70f282dd4884cc0822c
Varun Kadkade <varunkadkade2442@gmail.com>
2023-03-03 23:17:06 +0530

    add book model

---
+++ lib/models/book.dart
@@ -0,0 +1,19 @@
+class Book {
+  final String title;
+  final List<String> authors;
+  final String isbn;
+  final String condition;
+  final String description;
+  final List<String> categories;
+  final String coverImage;
+
+  Book(
+    this.title,
+    this.authors,
+    this.isbn,
+    this.condition,
+    this.description,
+    this.categories,
+    this.coverImage,
+  );
+}
